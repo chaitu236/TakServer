bdf823c26643bd858ba8dafa319fe4537decc31d
Chaitanya Vadrevu <chaitanya973@gmail.com>
2015-12-29 01:16:24 -0600

    Add mail library

--- src/tak/EMail.java
+++ src/tak/EMail.java
@@ -5,6 +5,9 @@
  */
 package tak;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -41,7 +44,13 @@ public class EMail {
     
     public static void main(String[] args) {
         System.out.println("Sending mail");
-        EMail.send(args[0], "test", "testing java mail");
-        System.out.println("Sent mail");
+        System.out.print("To:");
+        
+        try {
+            EMail.send(new BufferedReader(new InputStreamReader(System.in)).readLine(), "test", "testing java mail");
+            System.out.println("Sent mail");
+        } catch (IOException ex) {
+            Logger.getLogger(EMail.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
